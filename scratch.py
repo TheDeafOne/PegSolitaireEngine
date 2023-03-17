77d38f752d0e73a13dfb80f87139b1756d76dd6f
TheDeafOne <woodburnkb20@gcc.edu>
2023-03-17 18:20:04 -0400

    timing operations

--- scratch.py
+++ scratch.py
@@ -1,29 +1,11 @@
 from algorithms.backtracking.backtracking import Backtrack
 from board_logic.board import Board
+from visualization.vizualizer import Visualizer
+import time
 
-board = Board(5)
-# backtrack = Backtrack()
-# backtrack.backtrack(board)
-
-
-def print_state(state, board_size):
-    predent = " " * board_size
-    st = predent[1:]
-    fwtick = 1
-    cnt = 0
-    for i in range(board_size):
-        for j in range(i+1):
-            value = (j,i)
-            if cnt == fwtick:
-                fwtick += 1
-                cnt = 0
-                st += "\n" + predent[fwtick:]
-            st += str(state[value]) + " "
-            cnt += 1
-
-    
-    print(st)
-
-
-# for state in backtrack.solution_stack:
-#     print_state(state,board.board_size)
+board = Board(6)
+backtrack = Backtrack()
+start = time.time()
+backtrack.backtrack(board)
+end = time.time()
+print(end-start)
