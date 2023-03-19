8679ac82380565693b8042884f351822e69dcb80
TheDeafOne <woodburnkb20@gcc.edu>
2023-03-19 12:51:53 -0400

    removed algorithms

--- algorithms/algorithms.py
+++
@@ -1,9 +0,0 @@
-from .backtracking.backtracking import Backtrack
-class Algorithms:
-    def backtrack(self):
-        if live:
-            # manage live backtracking feed
-            pass
-        else:
-            return Backtrack()
-        
